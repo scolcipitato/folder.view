237af59cbc40f0f42c507af964183d29f2e1d407
scolcipitato <matteogozzi03@gmail.com>
2023-11-30 11:10:32 +0100

    fix vms dashboard

--- src/folder.view/usr/local/emhttp/plugins/folder.view/scripts/dashboard.js
+++ src/folder.view/usr/local/emhttp/plugins/folder.view/scripts/dashboard.js
@@ -120,7 +120,7 @@ const createFolders = async () => {
         const prom = await Promise.all(folderReq.vm);
         // Parse the results
         let folders = JSON.parse(prom[0]);
-        const unraidOrder = JSON.parse(prom[1]);
+        const unraidOrder = Object.values(JSON.parse(prom[1]));
         const vmInfo = JSON.parse(prom[2]);
         let order = JSON.parse(prom[3]);
     
