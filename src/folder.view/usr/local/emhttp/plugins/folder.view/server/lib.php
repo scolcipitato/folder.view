c5ca519f1a3f37b88654b8f1ce28c6af0fa6931e
scolcipitato <matteogozzi03@gmail.com>
2024-01-16 11:44:42 +0100

    2024.01.16.1

--- src/folder.view/usr/local/emhttp/plugins/folder.view/server/lib.php
+++ src/folder.view/usr/local/emhttp/plugins/folder.view/server/lib.php
@@ -226,7 +226,7 @@
 
             if (!empty($vms)) {
                 if (file_exists($user_prefs)) {
-                    $prefs = array_values(parse_ini_file($user_prefs)) ?: [];
+                    $prefs = parse_ini_file($user_prefs) ?: [];
                     $sort = [];
                     foreach ($vms as $vm) {
                         $sort[] = array_search($vm,$prefs);
@@ -237,7 +237,7 @@
                     natcasesort($vms);
                 }
 
-                $order = $vms;
+                $order = array_values($vms);
             }
         }
         return $order;
